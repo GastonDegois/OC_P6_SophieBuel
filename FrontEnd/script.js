9ee976b8132c477732842d461f47ab7e30ed4521
Gaston Degois <gaston.degois@gmail.com>
2023-04-28 10:44:05 +0200

    GALLERY'S FILTERS

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -5,6 +5,8 @@ async function fetchData(type) {
     return data;
 }
 async function fetchWorks() { return await fetchData('works'); }
+async function fetchCategories() { return await fetchData('categories'); }
+
 
 // GALLERY
 // build gallery at window's loading 
@@ -39,6 +41,44 @@ async function buildGallery(array) {
     const galleryContainer = document.querySelector(".gallery-container");
     galleryContainer.appendChild(gallery);
 }
+
+//FILTERS 
+// build filters buttons at window's loading
+window.addEventListener("DOMContentLoaded", buildFilters());
+
+// build filters buttons for the gallery from API categories
+async function buildFilters() {
+    const categories = await fetchCategories();
+    for(categorie of categories) {
+        const filters = document.querySelector(".filters");
+        const filter = document.createElement("button");
+        filter.classList.add(categorie.id);
+        filter.innerText = categorie.name;
+        filter.addEventListener("click", applyFilter);
+        filters.appendChild(filter);
+    }
+}
+
+// function to manage filter mehod in async function
+async function filterAsync (array, filterFunction) {
+    const boolArray = await Promise.all(array.map(filterFunction));
+    const arrayFiltered = array.filter((value, index) => boolArray[index]);
+    return arrayFiltered;
+}
+
+// use filter method on works array from API and then rebuild gallery with new array
+async function applyFilter(e) {
+    const filterId = await e.target.getAttribute('class');
+    let works = await fetchWorks();
+    const filterWorks = async (work) => {return work.categoryId == filterId}; 
+    worksFiltered = await filterAsync(works, filterWorks);
+    buildGallery(worksFiltered);
+}
+
+// re-build the full gallery buy clicking on the showAllBtn
+const showAllBtn = document.querySelector('.show-all-btn');
+showAllBtn.addEventListener('click', () => buildGallery(fetchWorks()));
+
 
 
 
